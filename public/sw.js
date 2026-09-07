9aaae73d76b5d9b838a030eae7a4b2d7da0dd4eb
Thomas Morris <thomasmorris@Mac.fios-router.home>
2026-09-07 12:43:33 -0400

    Simplify settings backup experience

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'garage-guide-shell-v7';
+const CACHE = 'garage-guide-shell-v8';
 self.addEventListener('install', (event) => {
   event.waitUntil(caches.open(CACHE).then((cache) => cache.add('./')));
   self.skipWaiting();
